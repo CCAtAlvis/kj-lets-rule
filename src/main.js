741b9ce0d4798a2d6bcfac35372b6ae8d42e06bc
Sanoop <sanoopmenon30@gmail.com>
2018-10-06 03:23:14 +0530

    Nav

--- src/main.js
+++ src/main.js
@@ -31,7 +31,7 @@ function createWindow() {
 
 	// and load the index.html of the app.
 	win.loadURL(url.format({
-		pathname: path.join(__dirname, 'index.html'),
+		pathname: path.join(__dirname, 'home.html'),
 		protocol: 'file:',
 		slashes: true
 	}));
